5707efcc0f747c147911efc89a83a7d028320bf2
Eelco Wijbrands <eelcowijbrands@gmail.com>
2018-11-12 14:31:42 +0100

    css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,8 +5,8 @@
 <head>
 
 
-    <link href="{{ asset('css/app.css') }}" media="all" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('css/main.css') }}" media="all" rel="stylesheet" type="text/css" />
+    <link href="{{ ('https://ancient-fortress-19731.herokuapp.com/css/app.css') }}" media="all" rel="stylesheet" type="text/css" />
+    <link href="{{ ('https://ancient-fortress-19731.herokuapp.com/css/main.css') }}" media="all" rel="stylesheet" type="text/css" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <title>@yield('title', 'NBA Clips')</title>
