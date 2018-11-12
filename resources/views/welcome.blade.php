75c429d10bdf047f2c1a9aeb4d828c69801c52b8
Eelco Wijbrands <eelcowijbrands@gmail.com>
2018-11-12 16:02:27 +0100

    FB plugin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,9 @@
     <h1 class="text-center">{{ $NBA }}</h1>
 
 <div class="section">
-    SECTION
+    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FNBAClips%2F&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" 
+    width="340" height="500" 
+    style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
 </div>
 <br>
 <div class="section">
