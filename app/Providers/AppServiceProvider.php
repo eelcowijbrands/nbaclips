e5a2d3ae8db4ff66ad426884f969b19679db772b
Eelco Wijbrands <eelcowijbrands@gmail.com>
2018-11-14 14:14:20 +0100

    Rik fix verwijderd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,9 +16,6 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        if(config('app.env') === 'production') {
-            \URL::forceScheme('https');
-        } 
     }
 
     /**
