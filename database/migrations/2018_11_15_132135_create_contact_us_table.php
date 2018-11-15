7d5b57acc9c8ce6792d8ea47ccf32c129cd74c16
Eelco Wijbrands <eelcowijbrands@gmail.com>
2018-11-15 16:52:50 +0100

    database connetion?

--- database/migrations/2018_11_15_132135_create_contact_us_table.php
+++ database/migrations/2018_11_15_132135_create_contact_us_table.php
@@ -13,7 +13,7 @@ class CreateContactUsTable extends Migration
      */
     public function up()
     {
-        Schema::create('contact_us', function (Blueprint $table) {
+        Schema::create('contactus', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name'); 
             $table->string('email'); 
