7533097d95519a3aa58da96a64075613e80ec6e1
Eelco Wijbrands <eelcowijbrands@gmail.com>
2018-11-16 15:57:41 +0100

    video added to homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,18 @@
 
 @section('content')
 
-    <h1 class="text-center">{{ $NBA }}</h1>
+    <!--<h1 class="text-center">{{ $NBA }}</h1>-->
+
+<!-- The video -->
+<video autoplay muted loop id="myVideo">
+    <source src="/svg/videohypenba.mp4" type="video/mp4">
+  </video>
+  
+  <!-- Optional: some overlay text to describe the video -->
+  <div class="content">
+    <h1>NBA Clips</h1>
+    <p>CATCH THE LATEST NBA NEWS</p>
+  </div>
 
 <div id="divRss"></div>
 
